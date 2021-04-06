43a41a796e99e69de7c011407170109bf894a158
Felipe Moreno Borges <felipe.b2014@gmail.com>
2021-04-06 11:45:06 -0300

    feat: Lista viagens.

--- alura-viagens/ViewController.swift
+++ alura-viagens/ViewController.swift
@@ -1,19 +1,25 @@
-//
-//  ViewController.swift
-//  alura-viagens
-//
-//  Created by caelum on 05/04/21.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    let listaViagens: [String] = ["Rio de Janeiro", "Ceará", "São Paulo"]
+    
+    @IBOutlet weak var tabelaViagens: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tabelaViagens.dataSource = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaViagens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = listaViagens[indexPath.row]
+        
+        return cell
+    }
 }
 
